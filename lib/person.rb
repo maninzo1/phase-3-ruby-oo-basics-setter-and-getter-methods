f96f63eff77c10431d431d354f4172539a5f15e1
MaryAninzo <aninzom211@gmail.com>
2021-10-13 09:16:48 -0400

    commit tree 5dec703b605e289352764e25cc733344c650ea40

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
-
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+    def job=(job)
+        @job = job
+    end
+    def job
+        @job
+    end
 end
+
+mary = Person.new
+mary.name = ("Mary")
+mary.job = ("Software Engineer Student")
+
+puts mary.name
+puts mary.job
